001dbee2663cb0e4f6aca950adc562fcbfd97eab
Michael Ramassamy <sunsmiley@live.fr>
2020-09-28 11:02:43 +0200

    Comment seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,19 +1,26 @@
-// command to seed : yarn md-seed run --dropdb
-// to add new seed without erasing previous data and reseeding same data user: yarn md-seed run exemples
+/** 
+ * Generate the data to be seeded
+ * Terminal command to launch the seed : yarn md-seed run --dropdb
+ * Terminal command to launch the seed whitout deleting the database (warning : could create double data) : yarn md-seed run
+ * Terminal command to add new seed (modelName could be 'users' or 'exemples' ...) without erasing previous data and reseeding same data user: yarn md-seed run modelName
+*/
 
 require('dotenv').config()
-
-import bcryptjs from 'bcryptjs'
-// import { hashPassword } from '../src/models/utils/passwordLib'
-
 const EMAIL = process.env.ADMIN_EMAIL
 const PASSWORD = process.env.ADMIN_PASSWORD
 const USERNAME = process.env.ADMIN_USERNAME
 
+/**
+ * Generate the data to be seeded
+ * @return { Object } data
+ * @return { Object[] } data.users
+ * @return { String } data.users[].email
+ * @return { String } data.users[].password
+ * @return { String } data.users[].username
+ * @return { Boolean } data.users[].roleAdmin
+ * @return { Object[] } data.exemples
+ */
 export function seed() {
-  // let hashedPassword = await hashPassword("xxxx")
-  // console.log('hashedPassword')
-  // console.log(hashedPassword)
   const data = {
     "users": [
       {
